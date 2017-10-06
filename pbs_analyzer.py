e2fff7b1472a1907e1c63d5a032ef3dcda88dde2
Eric Pascolo <eric.pascolo@cineca.it>
2017-10-06 17:59:30 +0200

    add bash function get_nodes_by_job and implement argparse and job query in pbs.py

--- pbs_analyzer.py
+++ pbs_analyzer.py
@@ -1,8 +1,45 @@
+## PARALLEL SHELL software
+##
+## author : eric pascolo
+##
+
 import sys
 import json
 import os
+import argparse
+
+def cl_parser():
+    """
+    Parsing command line pars with argparse module and return dictionary
+    """
+    
+    parser = argparse.ArgumentParser(description = '''
+    Python check suite to HPC Cluster''',
+    formatter_class=argparse.RawTextHelpFormatter
+    )
+
+    parser.add_argument(   '--job', 
+                            required = False,
+                            help = 'Given job name return list of nodes')
+
+    parser.add_argument(   '--queue', 
+                            required = False,
+                            help = 'Given queue name return list of nodes')  
+    args = parser.parse_args()
+    convdict = vars(args)
+    newdict =   dict([(vkey, vdata) for vkey, vdata in convdict.iteritems() if(vdata) ])
+    return newdict
+
+
+# init empty var
+nodesname_list = []
+nodes_selected = []
 filecontained = ''
 
+#load arguments
+args = cl_parser()
+
+#load json file
 try:
     if os.path.isfile("pbsout.json"):
         filecontained = json.loads(open("pbsout.json").read())
@@ -11,28 +48,46 @@ try:
 except:
     pass
 
-partofind = sys.argv[1]
 
-
-nodesname_list = []
-
+#load nodes name
 for i in filecontained["nodes"]:
     nodesname_list.append(i)
 
-nodes_selected = []
+
+# search loop
 for i in nodesname_list:
-    strings = filecontained["nodes"][i]["resources_available"]["Qlist"].split(",")
-    for n in strings:
-        if  partofind ==  n:
-            nodes_selected.append(i)
+
+    # queue search
+    if "queue" in args:
+        strings = filecontained["nodes"][i]["resources_available"]["Qlist"].split(",")
+        for n in strings:
+            if  args["queue"] ==  n:
+                nodes_selected.append(i)
+
+    # job search
+    elif "job" in args:
+
+        try:
+            jobs = filecontained["nodes"][i]["jobs"]
+            for n in jobs:
+                if  args["job"] ==  n:
+                    nodes_selected.append(i)
+        except:
+            pass
+        
+# write founded nodes on stdout
+if len(nodes_selected) > 0:
+    final_string = nodes_selected[0]
+
+    for i in nodes_selected[1:]:
+        final_string = final_string+","+i
+
+    number_of_nodes_ok = "NODES FOUND = "+str(len(nodes_selected))+"\n"
+
+    sys.stderr.write(number_of_nodes_ok)
+    print final_string
 
 
-final_string = nodes_selected[0]
-
-for i in nodes_selected[1:]:
-    final_string = final_string+","+i
-
-number_of_nodes_ok = "NODES FOUND = "+str(len(nodes_selected))+"\n"
-
-sys.stderr.write(number_of_nodes_ok)
-print final_string
+# write on std error if no nodes was found
+else: 
+    sys.stderr.write("ERROR : no nodes found\n")
--- pshell.sh
+++ pshell.sh
@@ -1,13 +1,11 @@
-#marconi nodes
-export nodes_knl=r113c15s01-hfi,r113c15s02-hfi,r113c15s03-hfi,r113c15s04-hfi,r113c16s01-hfi,r113c16s02-hfi,r113c16s03-hfi,r113c16s04-hfi,r113c17s01-hfi,r113c17s02-hfi,r113c17s03-hfi,r113c17s04-hfi,r113c18s01-hfi,r113c18s02-hfi,r114c15s01-hfi,r114c15s02-hfi,r114c15s03-hfi,r114c15s04-hfi,r114c16s01-hfi,r114c16s02-hfi,r114c16s03-hfi,r114c16s04-hfi,r114c17s01-hfi,r114c17s02-hfi,r114c17s03-hfi,r114c17s04-hfi,r114c18s01-hfi,r114c18s02-hfi
-
-export nodes_skl=r149c12s01-hfi,r149c12s02-hfi,r149c12s03-hfi,r149c12s04-hfi,r149c13s01-hfi,r149c13s02-hfi,r149c13s03-hfi,r149c13s04-hfi
-export nodes_bwd=r044c03s03,r044c03s04
-export nodes_total="${nodes_knl},${nodes_bwd},${nodes_skl}"
-
-
-
 #!/bin/bash
+
+## PARALLEL SHELL software
+##
+## author : eric pascolo
+##
+
+
 function pshell {
 ## interactive parallel shell
 for i in $(echo $1 | tr ',' '\n'); do echo "-------------------------"$i; ssh $i "${2}"; done
@@ -33,8 +31,33 @@ function pbs_get_nodes_by_queue {
 
 PSHELLDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)
 pbsnodes -a -Fjson > pbsout.json
-export "$1"=$(python $PSHELLDIR/pbs_analyzer.py $2)
+export "$1"=$(python $PSHELLDIR/pbs_analyzer.py --queue $2)
 rm pbsout.json
 
+}
+
+
+function pbs_get_nodes_by_job {
+## return node list given job name
+## take two input argument:
+##    - name of variable to store the nodelist
+##    - name of job
+##
+
+PSHELLDIR=$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)
+pbsnodes -a -Fjson > pbsout.json
+export "$1"=$(python $PSHELLDIR/pbs_analyzer.py --job $2)
+rm pbsout.json
+
+}
+
+
+
+function load_cineca_test_nodes_list {
+export nodes_knl=r113c15s01-hfi,r113c15s02-hfi,r113c15s03-hfi,r113c15s04-hfi,r113c16s01-hfi,r113c16s02-hfi,r113c16s03-hfi,r113c16s04-hfi,r113c17s01-hfi,r113c17s02-hfi,r113c17s03-hfi,r113c17s04-hfi,r113c18s01-hfi,r113c18s02-hfi,r114c15s01-hfi,r114c15s02-hfi,r114c15s03-hfi,r114c15s04-hfi,r114c16s01-hfi,r114c16s02-hfi,r114c16s03-hfi,r114c16s04-hfi,r114c17s01-hfi,r114c17s02-hfi,r114c17s03-hfi,r114c17s04-hfi,r114c18s01-hfi,r114c18s02-hfi
+
+export nodes_skl=r149c12s01-hfi,r149c12s02-hfi,r149c12s03-hfi,r149c12s04-hfi,r149c13s01-hfi,r149c13s02-hfi,r149c13s03-hfi,r149c13s04-hfi
+export nodes_bwd=r044c03s03,r044c03s04
+export nodes_total="${nodes_knl},${nodes_bwd},${nodes_skl}"
 
 }
